2b20419e07402e5172c73b3aaa732fec948eeb3e
wanghao <312939906@qq.com>
2019-04-15 11:37:11 +0800

    tools

--- app/src/main/java/com/example/septe/myapplication/fragment/BlankFragment3.java
+++ app/src/main/java/com/example/septe/myapplication/fragment/BlankFragment3.java
@@ -6,6 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.septe.myapplication.R;
 
@@ -29,7 +33,32 @@ public class BlankFragment3 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank_fragment3, container, false);
+        final View view = inflater.inflate(R.layout.fragment_blank_fragment3, container, false);
+        RadioGroup radioGroup = (RadioGroup) view.findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = (RadioButton) view.findViewById(checkedId);
+                Toast.makeText(view.getContext(), "按钮组发生改变，你选了：" + radioButton.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
+        //
+        Button btnchange = (Button) view.findViewById(R.id.btnpost);
+        final RadioGroup radioGroup1 = (RadioGroup) view.findViewById(R.id.radioGroup);
+        // 为radioGroup设置一个监听器 setOnCheckedChanged()
+        btnchange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < radioGroup1.getChildCount(); i++) {
+                    RadioButton rd = (RadioButton) radioGroup1.getChildAt(i);
+                    if (rd.isChecked()) {
+                        Toast.makeText(getContext(), "点击提交按钮，获取你选择的是：" + rd.getText(), Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                }
+            }
+        });
+        return view;
     }
 
 }
