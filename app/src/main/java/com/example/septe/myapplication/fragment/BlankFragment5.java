840619b4d6c87f099b405ae6a7058535d42f8fd9
wanghao <312939906@qq.com>
2019-04-16 10:35:50 +0800

    TimePicker DatePicker TextClock

--- app/src/main/java/com/example/septe/myapplication/fragment/BlankFragment5.java
+++ app/src/main/java/com/example/septe/myapplication/fragment/BlankFragment5.java
@@ -5,11 +5,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 import com.example.septe.myapplication.R;
 
+import java.util.Calendar;
 
-public class BlankFragment5 extends Fragment {
+
+public class BlankFragment5 extends Fragment implements DatePicker.OnDateChangedListener {
+
+    private View view;
 
     public BlankFragment5() {
         // Required empty public constructor
@@ -24,6 +30,18 @@ public class BlankFragment5 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank_fragment5, container, false);
+        view = inflater.inflate(R.layout.fragment_blank_fragment5, container, false);
+        DatePicker dp_test = (DatePicker) view.findViewById(R.id.dp_test);
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int monthOfYear = calendar.get(Calendar.MONTH);
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+        dp_test.init(year, monthOfYear, dayOfMonth, this);
+        return view;
+    }
+
+    @Override
+    public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        Toast.makeText(view.getContext(),"您选择的日期是："+year+"年"+(monthOfYear+1)+"月"+dayOfMonth+"日!",Toast.LENGTH_SHORT).show();
     }
 }
